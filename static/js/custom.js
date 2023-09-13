afd01c84db0e51ae281823d28f4876b26b1cf815
Luffykun1010 <muhammedsuhusuhail@gmail.com>
2023-09-13 15:00:39 +0530

    edit

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,15 +1,16 @@
 $(document).ready(function () {
     $('.increment-btn').click(function(e)  {
         e.preventDefault();
-        var inc_value = $(this).closest('.pd_data').find('.qty-input').val();
-        var prd_qty = $(this).closest('.pd_data').find('.prd_qty').val();
-        var value = parseInt(inc_value,prd_qty);
+        var dec_value = $(this).closest('.pd_data').find('.qty-input').val();
+        var product_qty = $(this).closest('.pd_data').find('.prd_quantity').val();
+        var value = parseInt(dec_value,10);
         // value =isNaN(value) ? 0 : value;
-        print(prd_qty)
-        if (value<prd_qty) {
+        if (value<product_qty) {
             value++;
             $(this).closest('.pd_data').find('.qty-input').val(value);
         }
+        // 
+        // value =isNaN(value) ? 0 : value;
     });
     $('.decrement-btn').click(function(e)  {
         e.preventDefault();
